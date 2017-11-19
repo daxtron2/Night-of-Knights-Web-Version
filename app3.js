4d5f2893094576ba0baf45c0e015be03b4b99a64
TJ Wolschon <tjw3948@g.rit.edu>
2017-11-18 21:42:31 -0500

    add Phaser's necessities to javascript files

---
+++ app3.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+var cursors;
+
+function preload(){
+    
+}
+
+function create(){
+
+}
+
+function update(){
+    
+}
