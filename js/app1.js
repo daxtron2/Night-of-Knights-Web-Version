010888de98bdd44156b16e50795304805bca34e6
TJ Wolschon <tjw3948@g.rit.edu>
2017-11-18 23:06:54 -0500

    added player, background, floor, player physics

--- js/app1.js
+++ js/app1.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1600, 900, Phaser.AUTO, '', { preload: preload, create: create, update: update },false,false);
 var cursors;
 
 function preload() {
@@ -7,7 +7,7 @@ function preload() {
     game.load.image("player", "images/player1.png");
     cursors = game.input.keyboard.createCursorKeys();
 }
-
+var player, floor;
 function create() {
     //initialize the physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -16,9 +16,26 @@ function create() {
     game.add.sprite(0, 0, "fog");
 
     //add the floor
-    game.add.sprite(0, game.world.height - 32, "floor");
+    floor = game.add.sprite(0, game.world.height - 134, "floor");
+    game.physics.arcade.enable(floor);
+    floor.body.immovable = true;
+    floor.body.setSize(1600,80,0,54);
+
+    //add the player
+    player = game.add.sprite(0,0,"player");
+
+    //scale the player
+    player.scale.setTo(5,5);
+
+    //enable physics for player
+    game.physics.arcade.enable(player);
+
+    //change player's physics settings
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
 }
 
 function update() {
-
+    game.physics.arcade.collide(player,floor);
 }
