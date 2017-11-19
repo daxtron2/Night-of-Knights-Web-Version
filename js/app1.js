ca1edcfa0a822d72bd16698f36c520030e95eca4
TJ Wolschon <tjw3948@g.rit.edu>
2017-11-18 22:40:05 -0500

    added more images

--- js/app1.js
+++ js/app1.js
@@ -1,14 +1,24 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var cursors;
 
-function preload(){
-    
+function preload() {
+    game.load.image("fog", "images/fog.png");
+    game.load.image("floor", "images/background_new.png");
+    game.load.image("player", "images/player1.png");
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
-function create(){
+function create() {
+    //initialize the physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    //add the background
+    game.add.sprite(0, 0, "fog");
+
+    //add the floor
+    game.add.sprite(0, game.world.height - 32, "floor");
 }
 
-function update(){
-    
+function update() {
+
 }
